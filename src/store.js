b6e09d0c0542660600159392c96f324c7d303a3a
Ricardo Matos de Azevedo Bossan <ricardobossan@gmail.com>
2019-04-05 17:52:51 -0300

    S11A67. Notify State & Alert Component

--- src/store.js
+++ src/store.js
@@ -4,7 +4,7 @@ import 'firebase/firestore';
 import { reactReduxFirebase, firebaseReducer } from 'react-redux-firebase';
 import { reduxFirestore, firestoreReducer } from 'redux-firestore';
 // Reducers
-// @todo
+import notifyReducer from './reducers/notifyReducer';
 
 const firebaseConfig = {
   // Initialize Firebase
@@ -38,7 +38,8 @@ const createStoreWithFirebase = compose(
 // Add firebase to reducers
 const rootReducer = combineReducers({
   firebase: firebaseReducer,
-  firestore: firestoreReducer // <- needed if using firestore
+  firestore: firestoreReducer, // <- needed if using firestore
+  notify: notifyReducer
 });
 
 // Create store with reducers and initial state
