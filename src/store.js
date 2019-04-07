ea7463f22bd9d12779a5915e6629280cbea4138e
Ricardo Matos de Azevedo Bossan <ricardobossan@gmail.com>
2019-04-07 15:17:44 -0300

    S11A69. Settings Reducer & Actions

--- src/store.js
+++ src/store.js
@@ -5,6 +5,7 @@ import { reactReduxFirebase, firebaseReducer } from 'react-redux-firebase';
 import { reduxFirestore, firestoreReducer } from 'redux-firestore';
 // Reducers
 import notifyReducer from './reducers/notifyReducer';
+import settingsReducer from './reducers/settingsReducer';
 
 const firebaseConfig = {
   // Initialize Firebase
@@ -39,7 +40,8 @@ const createStoreWithFirebase = compose(
 const rootReducer = combineReducers({
   firebase: firebaseReducer,
   firestore: firestoreReducer, // <- needed if using firestore
-  notify: notifyReducer
+  notify: notifyReducer,
+  settings: settingsReducer
 });
 
 // Create store with reducers and initial state
