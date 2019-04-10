cb75c1891796fa2433278a5e49c66070649f3e4c
Ricardo Matos de Azevedo Bossan <ricardobossan@gmail.com>
2019-04-10 18:49:21 -0300

    S11A73. Deploy To Firebase

---
+++ build/precache-manifest.9924482ac61d99cdc17b30c73b13aa91.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "f2c5716b2fc07391e80a",
+    "url": "/static/js/main.ac7d2fef.chunk.js"
+  },
+  {
+    "revision": "07f87173fdbe05419d53",
+    "url": "/static/js/2.78a2746a.chunk.js"
+  },
+  {
+    "revision": "f2c5716b2fc07391e80a",
+    "url": "/static/css/main.de0623ec.chunk.css"
+  },
+  {
+    "revision": "1d38851fcb0db44d2abdf595b71bb714",
+    "url": "/index.html"
+  }
+];
